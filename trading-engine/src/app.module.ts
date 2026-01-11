a3064df9a1f0978eb724c719199682a4b159a941
victor orlunda <vicorlunda@gmail.com>
2026-01-11 19:51:19 +0100

    chore: remove redundant imports

--- trading-engine/src/app.module.ts
+++ trading-engine/src/app.module.ts
@@ -4,14 +4,13 @@ import { FeedModule } from './feed/feed.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ExecutionModule } from './execution/execution.module';
 import { ArbitrageModule } from './arbitrage/arbitrage.module';
-import { PersistenceModule } from './persistence/persistence.module';
+
 
 @Module({
   imports: [
     FeedModule,
     ExecutionModule,
     ArbitrageModule,
-    PersistenceModule,
     EventEmitterModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
   ],
